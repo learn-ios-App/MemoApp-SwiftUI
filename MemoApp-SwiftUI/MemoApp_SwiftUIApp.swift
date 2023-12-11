5b8518e6756d238ec8813cbaa3187054c0c05a17
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-12-12 01:59:23 +0900

    Initial Commit

---
+++ MemoApp-SwiftUI/MemoApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoApp_SwiftUIApp.swift
+//  MemoApp-SwiftUI
+//
+//  Created by 渡邊魁優 on 2023/12/12.
+//
+
+import SwiftUI
+
+@main
+struct MemoApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
